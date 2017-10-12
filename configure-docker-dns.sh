0ff9033e5350f8f55b3d76587df9f06627173baa
eranga bandara <eranga.herath@pagero.com>
2017-10-12 07:51:36 +0530

    config dns docker

--- configure-docker-dns.sh
+++ configure-docker-dns.sh
@@ -2,7 +2,7 @@
 # host entry -> '10.4.1.4   dockerreigstry.pagero.local'
 # 1. run debian image
 # 2. check host entry exists in /etc/hosts file
-# 3. if not exists add it to /etc/hosts file 
+# 3. if not exists add it to /etc/hosts file
 docker run -it --privileged --pid=host debian nsenter \
     -t 1 -m -u -n -i sh \
     -c "if ! grep -q dockerregistry.pagero.local /etc/hosts; then echo -e '10.4.1.4\tdockerregistry.pagero.local' >> /etc/hosts; fi"
